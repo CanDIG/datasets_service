110be5913040290c1fb57e2ae09ff2b8515ae019
Dashaylan Naidoo <dashaylan@gmail.com>
2019-11-08 12:25:04 -0800

    Merge Initial Datasets Service Implementation  (#1)
    * Initial commit to check server start
    
    * Add auth module code from rnaget to comply with swagger spec
    
    * Add ORM methods
    
    * Fix data file location
    
    * Delete datasets.db
    
    * Add initial testing framework
    
    * Add tag/version tests
    
    * Add travis config
    
    * Add FieldEncoder class to apilog to handle datetime on 405s
    
    * Add wsgi configs
    
    * Change .yaml loading for uwsgi
    
    * remove print statements
    
    * Add location variable to uwsgi ini

---
+++ candig_dataset_service/orm/models.py
@@ -0,0 +1,52 @@
+"""
+SQLAlchemy models for database
+"""
+
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy import TypeDecorator
+from candig_dataset_service.orm.guid import GUID
+from candig_dataset_service.orm import Base
+import json
+
+
+class JsonArray(TypeDecorator):
+    """
+    Custom array type to emulate arrays in sqlite3
+    """
+
+    impl = String
+
+    def process_bind_param(self, value, dialect):
+        return json.dumps(value)
+
+    def process_result_value(self, value, dialect):
+        return json.loads(value)
+
+    def copy(self):
+        return JsonArray(self.impl.length)
+
+
+class Dataset(Base):
+    """
+    SQLAlchemy class representing datasets
+    """
+    __tablename__ = 'datasets'
+    id = Column(GUID(), primary_key=True)
+    version = Column(String(10), default="")
+    tags = Column(JsonArray(), default=[])
+    name = Column(String(100), unique=True, nullable=False)
+    description = Column(String(100), default="")
+    created = Column(DateTime())
+    __table_args__ = ()
+
+
+class ChangeLog(Base):
+    """
+    SQLAlchemy class for listing changes to the database with version update
+    """
+    __tablename__ = 'changelogs'
+    version = Column(String(10), primary_key=True)
+    log = Column(JsonArray())
+    created = Column(DateTime())
+    __table_args__ = ()
+
